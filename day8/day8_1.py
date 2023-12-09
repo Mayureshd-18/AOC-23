38b8f28b5e7a838b7c570f3e7ebadd65d7fc45b1
Mayureshd18 <mayuresh.dharwadkar211@vit.edu>
2023-12-09 14:39:40 +0530

    add comments

--- day8/day8_1.py
+++ day8/day8_1.py
@@ -1,11 +1,15 @@
 with open("input_1.txt") as f:
+    # Read the lines
     data = f.read()
     lines = data.splitlines()
 
 
     mapper = {}
 
+    # First line is the sequence
     seq = lines[0]
+
+    # After the first line and a new line we have the map
     for line in lines[2:]:
         src, dest = line.split("=")
         src = src.strip()
@@ -13,16 +17,21 @@ with open("input_1.txt") as f:
         dest[0] = dest[0].replace("(", "")
         dest[1] = dest[1].replace(")", "").strip()
 
+        # Store the source and possible destinations in a dict
         mapper[src] = dest
 
     print(mapper)
 
+    #The source is the node 'AAA' and the destination is node 'ZZZ'
     crt = 'AAA'
 
+    #To store the number of steps
     res = 0
+    #To cycle through the sequence
     ctr = 0
     n = len(seq)
 
+    #Continue finding the next step until we reach destination and simultaneously increment the steps needed and cycle through the sequence
     while crt!= 'ZZZ':
         crt = mapper[crt][1] if seq[ctr] == 'R' else mapper[crt][0]
         ctr = (ctr + 1)%n
--- day8/day8_2.py
+++ day8/day8_2.py
@@ -1,11 +1,15 @@
 import math
 with open("input_1.txt") as f:
+
+    #Read the lines
     data = f.read()
     lines = data.splitlines()
 
     mapper = {}
-
+    #First line is the sequence
     seq = lines[0]
+
+    #After the first line and a new line we have the map
     for line in lines[2:]:
         src, dest = line.split("=")
         src = src.strip()
@@ -13,10 +17,12 @@ with open("input_1.txt") as f:
         dest[0] = dest[0].replace("(", "")
         dest[1] = dest[1].replace(")", "").strip()
 
+        #Store the source and possible destinations in a dict
         mapper[src] = dest
 
     print(mapper)
 
+    #In part 2 find out all the nodes ending with 'A'
     start_nodes = [i for i in mapper if i[-1] == 'A']
     print(start_nodes)
 
@@ -24,8 +30,10 @@ with open("input_1.txt") as f:
     ctr = 0
     n = len(seq)
 
+    #List to store the turns required for every node to reach destination
     indis = []
 
+    #Calculate the turns required for each node
     for i in range(len(start_nodes)):
         while not start_nodes[i].endswith('Z'):
             start_nodes[i] = mapper[start_nodes[i]][1] if seq[ctr] == 'R' else mapper[start_nodes[i]][0]
@@ -36,6 +44,9 @@ with open("input_1.txt") as f:
         res = 0
 
     # print(indis)
+
+    #Finally find the LCM of all the individual turns.
+    #NOTE: Python 3.9+ supports *.
     print(math.lcm(*indis))
 
 
